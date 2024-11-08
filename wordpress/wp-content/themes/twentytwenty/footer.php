ba03f504a736e4ccb133a5a0ee3375b5676caf0a
maitrananhtuan <maituan5540@gmail.com>
2024-11-08 14:40:36 +0700

    update file

--- wordpress/wp-content/themes/twentytwenty/footer.php
+++ wordpress/wp-content/themes/twentytwenty/footer.php
@@ -167,7 +167,7 @@ get_header(); ?>
     <?php
     $args = array(
         'post_type' => 'post',
-        'posts_per_page' => 3
+        'posts_per_page' => 6
     );
     $query = new WP_Query($args);
 
